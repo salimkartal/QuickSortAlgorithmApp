b1704fe1981c50c65d9d733b1728203efc831f2b
Kartal <salimkartal@outlook.com.tr>
2020-10-16 14:50:39 +0300

    first commit

---
+++ deneme/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace deneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SIRALAMA = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 272);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 35);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 272);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 35);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(196, 272);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(54, 35);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox4.Location = new System.Drawing.Point(290, 272);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(49, 35);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(377, 272);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(53, 35);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(42, 372);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "1.ADIM";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SIRALAMA
+            // 
+            this.SIRALAMA.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.SIRALAMA.FormattingEnabled = true;
+            this.SIRALAMA.ItemHeight = 24;
+            this.SIRALAMA.Location = new System.Drawing.Point(196, 333);
+            this.SIRALAMA.Margin = new System.Windows.Forms.Padding(6);
+            this.SIRALAMA.Name = "SIRALAMA";
+            this.SIRALAMA.Size = new System.Drawing.Size(234, 124);
+            this.SIRALAMA.TabIndex = 8;
+            this.SIRALAMA.Tag = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 482);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Aciklama";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Image = global::deneme.Properties.Resources.quick8;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(471, 544);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.SIRALAMA);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "QUICK SORT UYGULAMASI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox SIRALAMA;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ deneme/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace deneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int tiklanma_sayisi = 0;
+        //zaman karmaşıklığı için
+        double timecmp,n;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int[] eldendizi = new int[5];
+            int sayi = Convert.ToInt32(textBox1.Text);
+            eldendizi[0] = sayi;
+            int sayi1 = Convert.ToInt32(textBox2.Text);
+            eldendizi[1] = sayi1;
+            int sayi2 = Convert.ToInt32(textBox3.Text);
+            eldendizi[2] = sayi2;
+            int sayi3 = Convert.ToInt32(textBox4.Text);
+            eldendizi[3] = sayi3;
+            int sayi4 = Convert.ToInt32(textBox5.Text);
+            eldendizi[4] = sayi4;
+
+            tiklanma_sayisi++;
+
+            Quick_Sort(eldendizi, 0, 5);
+            SIRALAMA.Items.Clear();
+            for (int i = 0; i < eldendizi.Length-4; i++)
+            {
+                if (tiklanma_sayisi == 1)
+                {
+                    SIRALAMA.Items.Add(eldendizi[i]);
+                    label1.Text = (textBox4.Text + " pivot secildi");
+                    button1.Text = "2.ADIM";
+                    
+                }
+
+            }
+            for (int i = 0; i < eldendizi.Length - 3; i++)
+            {
+                if (tiklanma_sayisi == 2)
+                {
+                    SIRALAMA.Items.Add(eldendizi[i]);
+                    label1.Text = "Sayilar sag ve sol alt gruba ayrıldı";
+                    button1.Text = "3.ADIM";
+                }
+
+            }
+            for (int i = 0; i < eldendizi.Length - 2; i++)
+            {
+                if (tiklanma_sayisi == 3)
+                {
+                    SIRALAMA.Items.Add(eldendizi[i]);
+                    label1.Text = "Sayilar kendi içlerinde karsilastirildi \nve sıralama devam ediyor";
+                    button1.Text = "4.ADIM";
+                }
+
+            }
+            for (int i = 0; i < eldendizi.Length - 1; i++)
+            {
+                if (tiklanma_sayisi == 4)
+                {
+                    SIRALAMA.Items.Add(eldendizi[i]);
+                    label1.Text = "Parçala ve fethet yöntemi ile her alt gruptada \npivot seciliyor ta ki sıralamayı elde edene kadar";
+                    button1.Text = "5.ADIM";
+                }
+
+            }
+            for (int i = 0; i < eldendizi.Length; i++)
+            {
+                if (tiklanma_sayisi == 5)
+                {
+                    SIRALAMA.Items.Add(eldendizi[i]);
+                    timecmp=Math.Pow(n,2)+n+12;
+                    button1.Text = "BİTTİ";
+                    label1.Text = "Time Complexity: n^2 + 3n +12 \nBig O Notasyonu: nlogn";
+                }
+            }
+
+            
+
+
+            textBox4.BackColor = Color.Yellow;
+            
+          
+        }
+
+       static int[] Quick_Sort(int[] dizi, int sol, int sag)
+        {
+            int p = dizi[sag - 1], i = sol, j = sag - 2, temp = 0;
+          
+            if (sag - sol > 2)
+            {
+                while (i < j)
+                {
+
+                    while (dizi[i] < p) { i++; }
+
+                    while (j > 0 && dizi[j] > p) { j--; }
+
+                    if (i < j)
+                    {
+                        temp = dizi[i];
+                        dizi[i++] = dizi[j];
+                        dizi[j--] = temp;
+                    }
+                }
+            }
+            //Pivot elemanı karşılaştır ve yer değiştir
+            if (p < dizi[i])
+            {
+                temp = dizi[i];
+                dizi[i] = dizi[sag - 1];
+                dizi[sag - 1] = temp;
+            }
+
+            //Solda eleman varsa
+            if (i - sol > 1)
+                dizi = Quick_Sort(dizi, sol, i);
+
+            //Sağda eleman varsa
+            if (sag - (i + 1) > 1)
+                dizi = Quick_Sort(dizi, i + 1, sag);
+
+            return dizi;
+        }
+    }
+}
